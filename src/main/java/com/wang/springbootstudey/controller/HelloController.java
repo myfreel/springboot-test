bb076386014024c3a698fd4261be1521a962ade6
myfreel <myfreel@163.com>
2020-02-11 21:13:37 +0800

    222

--- src/main/java/com/wang/springbootstudey/controller/HelloController.java
+++ src/main/java/com/wang/springbootstudey/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world ";
     }
 }
